b2ba440cca4eb7400e05262bcd891ba64a23cfdc
unknown <edwinedu@proton.me>
2025-06-13 22:03:57 +0300

    Explore datetime module

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+def get_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {formatted_date}")
+    return current_date
+
+def calculate_future_date(current_date, days_to_add):
+    future_date = current_date + timedelta(days=days_to_add)
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    print(f"Future date: {days_to_add} days: {formatted_future_date}")
+    return future_date
+
+def main():
+    current_date = get_current_datetime()
+    days_to_add = int(input("Enter the number of days to add: "))
+    future_date = calculate_future_date(current_date, days_to_add)
+    print(f"Future date after adding {days_to_add} days: {future_date.strftime('%Y-%m-%d')}")
+if __name__ == "__main__":
+    main()
